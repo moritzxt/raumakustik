85c3c2dc31b691acf6a0389ab142fe7837960c95
Moritz Thoemen <m.thoemen@tu-berlin.de>
2023-07-11 13:57:04 +0200

    first steps

--- pages/Vorauslegung.py
+++ pages/Vorauslegung.py
@@ -1,16 +1,24 @@
 import streamlit as st
+import copy
 from utils import usecase
+from stream import raum
+import pickle 
 
-st.set_page_config(page_title= 'Grobauslegung', layout='wide')
+fileObj = open('raum.obj', 'rb')
+raum_fine = pickle.load(fileObj)
+fileObj.close()
+
+st.set_page_config(page_title= 'Feinauslegung', layout='wide')
 with st.container():
-    st.title('Tool für die Grobauslegung des Raums')
+    st.title('Tool für die Feinauslegung einer Wandfläche Raums')
+    st.text('Führen Sie zunächst die generelle Berechnung für den Raum durch')
     st.divider()
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    use = st.selectbox('Usecase nach DIN 18041', options=usecase.keys())
-    min_lim =  min(usecase[use])
-    max_lim =  max(usecase[use])
+    wall = st.selectbox('Wähle die zu bearbeitende Hauptfläche aus', options=st.session_state['main_walls'])
+
 with col2:
-    vol = st.number_input('Volumen in m³', min_value=min_lim,
-                        max_value=max_lim, value=min_lim)
+    sub_surface_count = len(raum_fine.sub_surface[wall])
+    sub_wall_list = [f'Subflaeche {i+1}' for i in range(sub_surface_count)]
+    sub_wallmaterial = st.selectbox('Wähle die Subwandfläche aus', options=sub_wall_list)
