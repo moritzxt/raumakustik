3194739bf83a7389fd3b04a23fef00bc51b7adbd
Noah Jens <noah.jens@campus.tu-berlin.de>
2023-07-14 14:39:13 +0200

    minor changes

--- pages/User_guide.py
+++ pages/User_guide.py
@@ -11,4 +11,4 @@ with open("pages/user_guide.pdf", "rb") as f:
     pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
 
     # Embed the PDF in Streamlit using an HTML tag
-    st.markdown(f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="700" type="application/pdf">', unsafe_allow_html=True)
+    st.markdown(f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="700" type="application/pdf" download="user_guide.pdf">', unsafe_allow_html=True)
